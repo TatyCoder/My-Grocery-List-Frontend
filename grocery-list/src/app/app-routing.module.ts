260e306d17e3485d7af7aaa2d593c7d25ace786c
Tatiana Domingue <tatiana.domingue@protonmail.com>
2022-02-26 15:07:17 -0500

    GroceryList Component created and routing added

--- grocery-list/src/app/app-routing.module.ts
+++ grocery-list/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GroceryListComponent } from './grocery-list/grocery-list.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: UserComponent
+},
+{
+  path: 'user/:userId',
+  component: GroceryListComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- grocery-list/src/app/app.module.ts
+++ grocery-list/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { GroceryListComponent } from './grocery-list/grocery-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    GroceryListComponent
   ],
   imports: [
     BrowserModule,
